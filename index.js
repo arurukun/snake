0bbc49edda3f83e9c1637c485beeeaf282ec4496
Yukie Kawamoto <arurukun0306@gmail.com>
2022-11-14 19:00:29 +0900

    Draw apple

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const canvas=document.getElementById("game");
 const ctx=canvas.getContext("2d");
 
-let speed=7;
+let speed=4;
 
 let tileCount=20;
 let tileSize=canvas.width / tileCount;
 let headX=10;
 let headY=10;
+
+let appleX=10;
+let appleY=7;
  
 let xVelocity=0;
 let yVelocity=0;
@@ -14,13 +17,16 @@ let yVelocity=0;
 // game loop
 
 function drawGame(){
-    clearScreen();
+    drawScreen();
     changeSnakePosition();
+    checkAppleCollision();
+    drawapple();
     drawSnake();
-    setTimeout(drawGame,10000 / speed);
+    setTimeout(drawGame, 1000 / speed);
 }
 
-function clearScreen(){
+
+function drawScreen(){
     ctx.fillStyle= "black";
     ctx.fillRect(0,0,canvas.width,canvas.height);
 }
@@ -33,6 +39,18 @@ function drawSnake(){
 function changeSnakePosition(){
     headX=headX+xVelocity;
     headY=headY+yVelocity;
+
+}
+function drawapple(){
+    ctx.fillStyle="red";
+    ctx.fillRect(appleX * tileCount,appleY * tileCount,tileSize,tileSize);
+}
+
+function checkAppleCollision(){
+    if (appleX == headX && appleY == headY){
+        appleX = Math.floor(Math.random() * tileCount);
+        appleY = Math.floor(Math.random() * tileCount);
+    }
 }
 
 document.body.addEventListener("keydown",keyDown);
